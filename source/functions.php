8562c111d6acb6bbcb0816cf982086a2a3dcaa2b
Shuma Yoshioka <s64.stdio@gmail.com>
2014-09-01 00:00:49 +0900

    Update

--- source/functions.php
+++ source/functions.php
@@ -1,1 +1,24 @@
 <?php
+
+if( !function_exists('_s_setup') ):
+function _s_setup() {
+  add_theme_support( 'automatic-feed-links' );
+  
+  add_theme_support( 'html5', array(
+    'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
+  ) );
+  
+  
+}
+endif;
+add_action( 'after_setup_theme', '_s_setup' );
+
+function _s_scripts() {
+  wp_enqueue_style( '_s-style', get_stylesheet_uri() );
+  wp_enqueue_style( '_s-style-sass', get_template_directory_uri().'/css/default.css' );
+  
+  if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+    wp_enqueue_script( 'comment-reply' );
+  }
+}
+add_action( 'wp_enqueue_scripts', '_s_scripts' );
