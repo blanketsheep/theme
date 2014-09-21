4efcb049edafc4f035227e95df5b44a979d58220
Shuma Yoshioka <s64.stdio@gmail.com>
2014-09-21 19:03:12 +0900

    Update

--- footer.php
+++ footer.php
@@ -2,7 +2,4 @@
   <div class="container widgets">
     <?php dynamic_sidebar( 'sidebar_footer' ) ?>
   </div>
-  <div class="container copyright">
-    copyright
-  </div>
 </footer></div><!-- #wrapper --><!--<wp_footer>--><?php wp_footer(); ?><!--</wp_footer>--></body></html>
